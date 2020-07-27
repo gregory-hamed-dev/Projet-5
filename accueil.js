66395d330ef691507a2891e1977068b505d51069
Gregory HAMED <gregoryhamed@yahoo.fr>
2020-07-27 15:55:02 +0200

    configuration du js de la page produit pour envoyer la selection des produits dans le localstorage et mise en forme

--- accueil.js
+++ accueil.js
@@ -2,35 +2,27 @@
 const request = new XMLHttpRequest();
 request.open('get', 'http://localhost:3000/api/teddies', true);
 request.send();
-
- 
 request.onreadystatechange = function(data){
     if(this.readyState === 4  && this.status === 200){
-      
        const datas = JSON.parse(this.responseText);
        const main= document.getElementById("container")
-       console.log(data)
         // récupération des valeurs de chaque produit 
         for (let data of datas){
-            let color = data.colors;
             let id = data._id;
             let name = data.name;
             let price = data.price;
-            let desc = data.description;
             let picture = data.imageUrl;
-            console.log(id)
         //affichage des différents produits disponibles 
             let div= document.createElement("div")
             div.classList.add('card')
             div.innerHTML=`<img src= "${picture}" class="card-img-top" alt="...">
-            <div class="card-body" id="card-firstproduct">
+            <div class="card-body">
             <h3 class="card-title text-center">${name}</h3>
             <p class="text-center"><span class="teddy-price">${price}€</span></p>
             <p class ="button text-center"><a href="./pages/teddy.html?id=${id}"><i class="fas fa-paw"></i>Voir le produit</a></p>`
             main.append(div)   
         }      
-    }
-    
+    } 
 } 
 
 
--- teddy.js
+++ teddy.js
@@ -1,11 +1,67 @@
-// URL de l'api
-const url = "http://localhost:3000/api/teddies";
-// Recupere les paramètres de l'url
-const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const url = "http://localhost:3000/api/teddies/"
+const getId = new URLSearchParams(window.location.search).get("id");
+const mainContainer = document.getElementById("main-container")
+//déclaration de la fonction principale de la page
+const Product = async () => {
+    const data = await teddy(url, getId);
+    TeddyDetails(data);  
+};
+//fonction pour enter en contact avec l'api
+const teddy = async (teddyUrl, teddyId) => {
+    const response = await fetch(teddyUrl + teddyId);
+    return await response.json(); 
+};
+//fonction qui récupére les informations détaillés du produit demandé par l'utilisateur
+const TeddyDetails = (Data) => {
+    mainContainer.innerHTML = `
+        <div class="product">
+            <div class="information">
+                <p class ="button text-center" style="width: 25%">
+                    <a href="../index.html"><i class="fas fa-paw"></i>Revenir à l'accueil</a>
+                </p>
+                <h2 class="product-title">${Data.name}</h2>
+                <img class="display-picture" src="${Data.imageUrl}" alt="${Data.name}">
+                <p class="price-here">${Data.price}€</p>
+                <p class="description">${Data.description}</p>
+                <div class="list">
+                    <label for="color-select"><i class="fas fa-palette"></i>Choix de la couleur :</label>
+                    <select name="colors" class="color-list"></select>
+                </div>
+                <button class="panier">Ajouter au panier</button>
+            </div>
+        </div>`
+    for (let colour of Data.colors){
+        document.querySelector(".color-list").appendChild(document.createElement("option")).innerText = colour;
+    };
+            
+    document.querySelector(".panier").addEventListener("click", (e) =>{
+                
+        document.querySelector(".badge-danger").textContent = this.value ++;
+        let colorChoise = document.querySelector(".color-list").value
+        const shopList = {
+            id: Data._id,
+            picture: Data.imageUrl,
+            name: Data.name,
+            color: colorChoise,
+            price: Data.price,
+            quantity: 1,
+        }
+       let panier = JSON.parse(localStorage.getItem("cardSelection"))
 
-const main = document.getElementById("container");
-console.log(id)
-// Affiche le produit
+       if (panier === null){
+           panier = {}
+       }
+       if (panier[shopList.id] !== undefined){
+        panier[shopList.id] ++; 
+       }
+       else{
+           panier[shopList.id] = shopList;
+       }
+       localStorage.setItem("cardSelection", JSON.stringify(panier))
+    })
+    
+}
+    
 
+Product()
 
